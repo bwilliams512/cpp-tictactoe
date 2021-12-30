08b5b8012808952f952470b7928260374cf346e3
Brian Williams <49660533+bwilliams512@users.noreply.github.com>
2021-12-30 14:17:59 -0600

    Add files via upload

---
+++ functions_ttt.hpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+void introduction();
+bool is_winner();
+bool filled_up();
+void empty();
+void set_position();
+void update_board();
+void change_player();
+void next_move();
+void end_game();
---
+++ ttt.cpp
@@ -0,0 +1,20 @@
+/*
+Codecademy
+Learn C++
+
+This is a Tic-Tac-Toe program.
+*/
+
+#include <iostream>
+#include "functions_ttt.hpp"
+
+int main() {
+
+  introduction();
+  
+  next_move();
+
+  end_game();
+    
+}
+
